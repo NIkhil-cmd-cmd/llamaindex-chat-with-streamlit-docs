54151688524efcf703e096b902c529bc7e8c25cc
Nikhil Krishnaswamy <krishnaswamynikhil@gmail.com>
2024-06-26 12:30:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ try:
 except ImportError:
     from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
 
-st.set_page_config(page_title="Chat with the Mental Health Assistant, powered by LlamaIndex", page_icon="🧠", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Share-On", page_icon="💬", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Chat with the Mental Health Assistant, powered by LlamaIndex 💬🧠")
+st.title("Chat with the Mental Health Assistant, powered by LlamaIndex")
 st.info("This chatbot is here to help answer your mental health questions and provide support. Please note that it is not a substitute for professional advice or therapy.", icon="📃")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
