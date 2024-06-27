ab3d7ef74a50cf84e9278f4b04b48c849c44f133
agithub-user <sharonlu314@gmail.com>
2024-06-27 11:44:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,7 +93,6 @@ def advocacy():
     st.title("Advocacy")
     st.info("At Share-On, we are dedicated to harnessing the power of user data to drive positive change. Through a careful and respectful approach, we utilize the insights gained from the stories shared within our organization to advocate for stronger mental health laws and bills. Our commitment to your privacy will always stand; every piece of information shared is treated with the utmost confidentiality. Your stories aren't just stories here; they're fuel for change. Our aim is to shape policies that truly help people dealing with mental health struggles.")
     # Add your resource content here
-
 import streamlit as st
 
 st.set_page_config(initial_sidebar_state="collapsed")
